411e32ae58a0cd26b6c5911ff0c5240d2dafd6ec
Cmuss33 <connormusson@gmail.com>
2026-01-07 17:18:40 -0500

    update backend to deploy

--- backend/tcgai_backend/tcgai_backend/settings.py
+++ backend/tcgai_backend/tcgai_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9htr%0mjaiefv0*q!$(is&9cn!b(c89iti-gw1c(u%)io2tr*$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', '*'] # Allow all for now
 
 
 # Application definition
